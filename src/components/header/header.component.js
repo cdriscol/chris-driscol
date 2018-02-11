3c06492bc460b6a2767a095cd4cb51a719f97da2
cdriscol <drizkol@gmail.com>
2018-02-11 13:18:43 -0700

    Learn about me button should animate scroll

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import classNames from 'classnames';
+import { ScrollLink } from '../common';
 import './header.css';
 
 export default function Header() {
@@ -10,12 +11,12 @@ export default function Header() {
         <div className="intro-text">
           <div className="intro-lead-in">Welcome To My Website!</div>
           <div className="intro-heading">{`It's Nice To Meet You`}</div>
-          <a
-            href="#aboutme"
+          <ScrollLink
+            to="aboutme"
             className={classNames('page-scroll', 'btn', 'btn-xl')}
           >
             Learn about me
-          </a>
+          </ScrollLink>
         </div>
       </div>
     </header>
