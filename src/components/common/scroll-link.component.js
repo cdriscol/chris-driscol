982e9c4166682262cca95ad4a89d06ceaf480fb4
cdriscol <drizkol@gmail.com>
2018-02-11 01:24:40 -0700

    Add events for scroll

--- src/components/common/scroll-link.component.js
+++ src/components/common/scroll-link.component.js
@@ -1,17 +1,24 @@
 // @flow
 import * as React from 'react';
 import { Link } from 'react-scroll';
+import ReactGA from 'react-ga';
 
 type Props = {
   children: React.Node,
 };
 
 export default function ScrollLink({ children, ...extraProps }: Props) {
+  const handleSetActive = (to: string) =>
+    ReactGA.event({
+      category: 'Scroll',
+      action: to,
+    });
   return (
     <Link
       style={{ cursor: 'pointer' }}
       smooth="easeInOutQuart"
       duration={1500}
+      onSetActive={handleSetActive}
       {...extraProps}
     >
       {children}
