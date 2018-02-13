34953787554165a73db3a59cec4fd112343b854d
cdriscol <drizkol@gmail.com>
2018-02-13 00:01:10 -0700

    Adding default variables and query changes

--- src/components/resume/index.js
+++ src/components/resume/index.js
@@ -28,15 +28,55 @@ export default class ResumePage extends React.Component<{}, State> {
         <GraphiQL
           fetcher={graphQLFetcher}
           editorTheme={'dracula'}
+          variables={`${JSON.stringify(
+            {
+              input: {
+                from: '',
+                name: '',
+                subject: '',
+                body: '',
+              },
+            },
+            null,
+            '\t',
+          )}`}
           query={`
-# Welcome to my Resume GraphiQL Explorer
-query {
+# Welcome to my Resume GraphiQL Explorer.
+# You can browse my schema by looking at the "Docs" on the right.
+query resume {
   chris {
     title
+    description
+    social {
+      github
+      linkedIn
+      email
+    }
+    experience {
+      location
+      title
+      duration
+      description
+    }
+    skills {
+      technologies
+    }
+  }
+}
+
+# Edit the "Query Variables" below, and send me an email using this mutation.
+# (make sure you to include your email in the from field)
+mutation contactMeMutation($input:ContactMeInput!) {
+  contactMe(input:$input) {
+    success
   }
 }`}
         >
-          <GraphiQL.Logo>Chris Driscol</GraphiQL.Logo>
+          <GraphiQL.Logo>
+            <a href="/" title="Chris Driscol">
+              Chris Driscol
+            </a>
+          </GraphiQL.Logo>
         </GraphiQL>
       </div>
     );
--- src/graphql/types/root.graphql.js
+++ src/graphql/types/root.graphql.js
@@ -7,9 +7,12 @@ import { nodeField } from './node-interface';
 
 export default new GraphQLObjectType({
   name: 'ROOT',
+  description:
+    'This is the root query of my GraphQL schema. This is the API that serves data for my website.',
   fields: {
     chris: {
       type: GraphQLNonNull(ChrisType),
+      description: 'This field contains information about me, Chris Driscol!',
       resolve: () => {
         const model = TypeModelResolver.getModelFromGraphType(ChrisType.name);
         invariant(model, 'Expected a model to match type name');
