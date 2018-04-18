6784067fed452c663162af4db2561219530a5e2d
cdriscol <drizkol@gmail.com>
2018-04-17 21:45:59 -0600

    Add comment tag

--- src/server.js
+++ src/server.js
@@ -80,6 +80,18 @@ app.use(async (req, res, next) => {
 
   res.status(status).send(`
 <!DOCTYPE html>
+<!--
+This site was created by..
+ .d8888b.  888              d8b               8888888b.          d8b                           888
+d88P  Y88b 888              Y8P               888  "Y88b         Y8P                           888
+888    888 888                                888    888                                       888
+888        88888b.  888d888 888 .d8888b       888    888 888d888 888 .d8888b   .d8888b .d88b.  888
+888        888 "88b 888P"   888 88K           888    888 888P"   888 88K      d88P"   d88""88b 888
+888    888 888  888 888     888 "Y8888b.      888    888 888     888 "Y8888b. 888     888  888 888
+Y88b  d88P 888  888 888     888      X88      888  .d88P 888     888      X88 Y88b.   Y88..88P 888
+ "Y8888P"  888  888 888     888  88888P'      8888888P"  888     888  88888P'  "Y8888P "Y88P"  888
+feel free to take a look around!
+-->
   <html>
     <head>
       ${head.base.toString()}
